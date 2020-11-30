8199b4e3bdd8c188a7fc6583ba2bdaa5203555ac
Bradley O'Leary <bradley_oleary@outlook.com>
2020-11-30 11:05:19 -0500

    Ex 1 Completed

--- client/src/components/AppContext.js
+++ client/src/components/AppContext.js
@@ -2,6 +2,17 @@ import React, { createContext, useEffect, useState } from 'react';
 
 export const AppContext = createContext(null);
 
+// My web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyC53FSTKspC2uPNiqeT5l22s4bGvmHM8yo',
+  authDomain: 'user-app-71ec8.firebaseapp.com',
+  databaseURL: 'https://user-app-71ec8.firebaseio.com',
+  projectId: 'user-app-71ec8',
+  storageBucket: 'user-app-71ec8.appspot.com',
+  messagingSenderId: '895828076293',
+  appId: '1:895828076293:web:3d5801968aaa3b46c6396c',
+};
+
 const AppProvider = ({ children }) => {
   return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
